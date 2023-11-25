5c2fae066bc75b4e71553b5a0c145edfb6dfa739
russlandy <ruslandabysov@gmail.com>
2023-11-25 22:57:54 +0300

    fix some functions and add features on page

--- Index.js
+++ Index.js
@@ -84,12 +84,17 @@ function selectWord(event) {
       ].addEventListener("mouseover", selectWord);
     }
   }
-  console.log(word);
+  // console.log(word);
 }
 
-function hideSelection() {
-  const rows = document.querySelectorAll(".boggle .row");
+let arr = []; // сюда пушатся слова если они существуют
 
+function hideSelection() {
+  // console.log(word); // здесь как раз у нас полностью выбранное слово получается, а не промежуточные варианты
+  const finishWord = word;
+  // console.log(finishWord);
+  const rows = document.querySelectorAll(".boggle .row");
+  selectedElements = []; // кажется при завершении выделения слова надо обнулять массив с выделенными буквами
   rows.forEach((row) => {
     const buttons = row.querySelectorAll(".button--boggle");
     buttons.forEach((btn) => {
@@ -98,6 +103,36 @@ function hideSelection() {
       btn.removeEventListener("mouseover", selectWord);
     });
   });
+  let wordGet = finishWord;
+  // console.log(finishWord); //сюда нужно вставлять слово из полученного на страничке
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordGet}`;
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Ошибка при выполнении запроса:", error);
+    }
+  }
+  function dataShow(data) {
+    if (data) {
+      const [word] = data;
+      let checkWord = word.word;
+      arr.push(word.word);
+      console.log(arr);
+      const rightWords = document.querySelector(".right-words");
+      const rightWord = document.createElement("div");
+      rightWord.innerHTML = `<div class="word"><span class="word-header">${checkWord}</span><span class="word-length"></span></div>`;
+      rightWords.appendChild(rightWord);
+    } else {
+      console.log("Такого слова не существует!");
+    }
+  }
+  fetchData(url).then((data) => dataShow(data));
 }
 
 const storedUsers = [
@@ -132,28 +167,29 @@ clearBtn.addEventListener("click", () => {
 // проверка слова в словаре
 
 // пока попытки работы с API
-let wordGet = word; //сюда нужно вставлять слово из полученного на страничке
-let arr = []; // сюда пушатся слова если они существуют
-let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordGet}`;
-async function fetchData(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
-  }
-}
-function dataShow(data) {
-  if (data) {
-    const [word] = data;
-    arr.push(word.word);
-    console.log(arr);
-  } else {
-    console.log("Такого слова не существует!");
-  }
-}
-fetchData(url).then((data) => dataShow(data));
+// let wordGet = finishWord;
+// console.log(finishWord); //сюда нужно вставлять слово из полученного на страничке
+// let arr = []; // сюда пушатся слова если они существуют
+// let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordGet}`;
+// async function fetchData(url) {
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Ошибка при выполнении запроса:", error);
+//   }
+// }
+// function dataShow(data) {
+//   if (data) {
+//     const [word] = data;
+//     arr.push(word.word);
+//     console.log(arr);
+//   } else {
+//     console.log("Такого слова не существует!");
+//   }
+// }
+// fetchData(url).then((data) => dataShow(data));
